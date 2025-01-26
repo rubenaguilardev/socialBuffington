144a5b022371f2cfb74b2c6c8969e103e0fd794e
rubenaguilardev <ragui56@wgu.edu>
2025-01-25 23:00:12 -0800

    Rework home page design

--- js/app.js
+++ js/app.js
@@ -3,23 +3,23 @@ function main() {
     // Skills Section
 
     let changeImage = document.getElementById("skillsBackground");
-    let changeSkillsH2 = document.querySelector(".scrollH2");
-    let changeSkillsP = document.querySelector(".scrollP");
-    let changeSkillsButton = document.querySelector(".skillsButton")
-    let skillsLeftScroll = document.getElementById("leftScroll");
-    let skillsRightScroll = document.getElementById("rightScroll");
+    let changeSkillsH2 = document.querySelector(".skillsHeading");
+    let changeSkillsP = document.querySelector(".skillsParagraph");
+    let changeSkillsButton = document.querySelector(".skillsButton");
+    let skillsLeftButton = document.getElementById("skillsLeftButton");
+    let skillsRightButton = document.getElementById("skillsRightButton");
     let i = 0;
 
 
 
 
-    skillsRightScroll.addEventListener('click', function() {
+    skillsRightButton.addEventListener('click', function() {
         i++;
         if (i > 3) i = 0;
         changeSkill(i);
     });
 
-    skillsLeftScroll.addEventListener('click', function() {
+    skillsLeftButton.addEventListener('click', function() {
         i--;
         if (i < 0) i = 3;
         changeSkill(i);
@@ -29,19 +29,34 @@ function main() {
 
 function changeSkill(index) {
 
+    changeImage.classList.remove('show');
+    changeSkillsH2.classList.remove('show');
+    changeSkillsP.classList.remove('show');
+    changeSkillsButton.classList.remove('show');
+
     setTimeout(function () {
         changeImage.style.backgroundImage = `url('img/skills${index}.jpg')`;
+
+        const parentAnchor = changeSkillsButton.closest('a');
 
         switch (index) {
             case 1:
                 changeSkillsH2.innerText = "Photography"
                 changeSkillsP.innerText = "Capturing moments with artistic flair and emotional resonance";
-                changeSkillsButton.innerText = "View My Photography"
+                changeSkillsButton.innerText = "View My Photographs"
+                if (parentAnchor) {
+                    parentAnchor.href = "photos.html";
+                }
+                break;
                 break
             case 2:
                 changeSkillsH2.innerText = "Event Marketing"
                 changeSkillsP.innerText = "Campaigns that engage and amplify brand stories";
                 changeSkillsButton.innerText = "View My Work"
+                if (parentAnchor) {
+                    parentAnchor.href = "work.html";
+                }
+                
                 break
             case 3:
                 changeSkillsH2.innerText = "Food & Beverage"
@@ -51,7 +66,17 @@ function changeSkill(index) {
             default:
                 changeSkillsH2.innerText = "Creative Storytelling"
                 changeSkillsP.innerText = "Crafted to captivate and inspire";
+                changeSkillsButton.innerText = "View My Work"
+                if (parentAnchor) {
+                    parentAnchor.href = "work.html";
+                }
         }
+
+        changeImage.classList.add('show');
+        changeSkillsH2.classList.add('show');
+        changeSkillsP.classList.add('show');
+        changeSkillsButton.classList.add('show');
+
     }, 500)
     
 }
